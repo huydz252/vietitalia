5ea1dfb5438d2a66e92dfa972b54f83cbbb21b81
Trần Quang Huy <252quanghuy@gmail.com>
2026-07-15 16:36:33 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,14 @@ import { renderMarketMatch } from "../components/sections/MarketMatch.js";
 import { renderAboutUs } from "../components/sections/AboutUs.js";
 import { renderNews } from "../components/sections/News.js";
 import { renderPartners } from "../components/sections/Partners.js";
+import { renderVideoIntro } from "../components/sections/VideoIntro.js";
 import { t } from "../i18n/i18n.js";
 export default function Home(container) {
   renderHero(container, t);
   renderMarketMatch(container, t);
   renderAboutUs(container);
 
-  // renderVideoIntro(container);
+  renderVideoIntro(container);
   renderNews(container);
   renderPartners(container, { compact: true });
 }
