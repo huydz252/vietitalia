94cada925ffc254cf8e9a8c5206524b126a79ad3
Trần Quang Huy <252quanghuy@gmail.com>
2026-08-01 09:51:42 +0700

    fix build admin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,2 +1,13 @@
-import { defineConfig } from "vite";
-export default defineConfig({});
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'), 
+      },
+    },
+  },
+});
